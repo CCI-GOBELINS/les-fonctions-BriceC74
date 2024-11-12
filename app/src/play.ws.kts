77b70fc36e0aa6f1f84336f9096753f5226f4641
BriceC74 <brice.chaves74@gmail.com>
2024-11-12 12:47:48 +0100

    feat(2): Fibonacci

--- app/src/play.ws.kts
+++ app/src/play.ws.kts
@@ -1,10 +1,10 @@
-fun evenOrOdd(type: String ,amount: Int) {
+fun evenOrOdd(type: String, amount: Int) {
     println(type)
 
     for (i in 1..amount) {
         when (type) {
             "even" -> if (i % 2 == 0) println(i)
-            "odd" -> if(i % 2 != 0) println(i)
+            "odd" -> if (i % 2 != 0) println(i)
         }
     }
 }
@@ -12,3 +12,17 @@ evenOrOdd("even", 10)
 evenOrOdd("odd", 10)
 
 
+fun fibonacci(amount: Int) {
+    println("Fibonacci")
+
+    var prev = 0
+    var step = 1
+    for (i in 1..amount) {
+        var num = prev + step
+        println(num)
+
+        prev = step
+        step = num
+    }
+}
+fibonacci(10)
